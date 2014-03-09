452639812bd499524c0b65d1826014d88856e441
Michael Zhou <mzhou@cse.unsw.edu.au>
2014-03-09 11:43:24 +1100

    Use Template Haskell to compute distance look-up table

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,28 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, TypeSynonymInstances #-}
 
 module Main where
 
+import Data.Array.Unboxed
 import Data.Default (def)
 import Data.Text (Text, pack, unpack)
+import Data.Word (Word16, Word32)
 import JavaScript.JQuery
 import Language.Haskell.TH.Syntax (lift)
 
 import Opal.Distance
 import Opal.Fare.Train
 
-stationOptions = $(lift $ foldr (++) "" (map (\x -> "<option value=\"" ++ x ++ "\">" ++ x ++ "</option>") (map show stations)))
+stationOptions = $(lift $
+    foldr (++) "" (map (\x -> "<option value=\"" ++ x ++ "\">" ++ x ++ "</option>") (map show stations))
+    )
+
+distanceList = $(lift $ concat [[trackDistance orig dest | dest <- stations] | orig <- stations])
+
+distances = let
+    numStations = fromIntegral $ 1 + fromEnum (maxBound :: Station)
+    bounds = (0, numStations * numStations)
+    in
+    listArray bounds distanceList :: UArray Word16 Word32
 
 main = do
     origElt <- select "#orig"
@@ -29,9 +41,10 @@ recalculate _ = do
     destVal <- select "#dest" >>= getVal
 
     let
-        orig = read $ unpack origVal
-        dest = read $ unpack destVal
-        distance = trackDistance orig dest
+        orig = read $ unpack origVal :: Station
+        dest = read $ unpack destVal :: Station
+        -- distance = trackDistance orig dest
+        distance = fromIntegral $ distances ! (fromIntegral $ (1 + fromEnum (maxBound :: Station)) * (fromEnum orig) + (fromEnum dest))
         peak = fare Peak distance
         offPeak = fare OffPeak distance
         in do
